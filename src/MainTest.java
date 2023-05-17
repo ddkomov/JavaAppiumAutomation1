7f9b8f4ce116cac4a01d08da6017a38b1ea5134f
ddkomov <ddkomov@sberbank.ru>
2023-05-17 19:30:37 +0300

    11. Статические поля и методы

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,14 +1,12 @@
 import org.junit.Test;
 
 public class MainTest extends CoreTestCase {
-    MathHelper Math = new MathHelper();
+   @Test
+    public void myFirstTest() {
 
-    @Test
-    public void myFirstTest () {
-        int a = Math.multiply(5);
-        System.out.println(a);
-        int b = Math.multiply(10,15);
-        System.out.println(b);
-    }
+   }
+   @Test
+   public void mySecondTest() {
 
+   }
 }
