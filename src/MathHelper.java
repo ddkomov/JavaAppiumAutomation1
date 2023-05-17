c4b2f242cfa94557815c0e82707c73e7c808211a
ddkomov <ddkomov@sberbank.ru>
2023-05-17 18:17:34 +0300

    09. Наследование

---
+++ src/MathHelper.java
@@ -0,0 +1,8 @@
+public class MathHelper {
+    public int multiply(int number) {
+        return number * 2;
+    }
+    public int multiply(int number, int multiplier) {
+        return number * multiplier;
+    }
+}
