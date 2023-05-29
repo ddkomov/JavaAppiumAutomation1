a540dc2c9ca43cc6e3a5f63d8ebd30266822a393
Dmitry <komov93@gmail.com>
2023-05-29 21:22:12 +0300

    смена пути к апк

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -19,7 +19,7 @@ public class FirstTest {
         capabilities.setCapability("automationName","Appium");
         capabilities.setCapability("appPackage","org.wikipedia");
         capabilities.setCapability("appActivity",".main.MainActivity");
-        capabilities.setCapability("app","C:/Users/Asus/IdeaProjects/JavaAppiumAutomation/apks/org.wikipedia.apk");
+        capabilities.setCapability("app","C:/Users/Asus/IdeaProjects/JavaAppiumAutomation1/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
